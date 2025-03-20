bee6116f693b7c3d45e879d5a1e81dfdcd5ac7f1
sunny-luoyun <2402580433@qq.com>
2025-03-20 08:34:07 +0800

    优化输出文件夹

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ def prep(path, list):
         start_timee = time.time()
 
         process = os.popen(
-            f'mkdir -p {path}/work/{i}')
+            f'mkdir -p {path}/work/preprocess/{i}')
         output = process.read()
         print(output)
         process.close()
@@ -15,42 +15,42 @@ def prep(path, list):
         print(f"现在开始处理{i}")
         # 转换mif /1s
         print('格式转换')
-        process = os.popen(f'mrconvert -fslgrad {path}/pre/{i}/{i}dwi.bvec {path}/pre/{i}/{i}dwi.bval {path}/pre/{i}/{i}dwi.nii.gz {path}/work/{i}/dwi_raw.mif -force')
+        process = os.popen(f'mrconvert -fslgrad {path}/pre/{i}/{i}dwi.bvec {path}/pre/{i}/{i}dwi.bval {path}/pre/{i}/{i}dwi.nii.gz {path}/work/preprocess/{i}/dwi_raw.mif -force')
         output = process.read()
         print(output)
         process.close()
 
         # 降噪 /19s
         print('降噪')
-        process = os.popen(f'dwidenoise {path}/work/{i}/dwi_raw.mif {path}/work/{i}/dwi_raw_denoise.mif -noise {path}/work/{i}/noiselevel.mif -force')
+        process = os.popen(f'dwidenoise {path}/work/preprocess/{i}/dwi_raw.mif {path}/work/preprocess/{i}/dwi_raw_denoise.mif -noise {path}/work/preprocess/{i}/noiselevel.mif -force')
         output = process.read()
         print(output)
         process.close()
 
         # 消除Gibbs Ring /23s
         print('消除Gibbs Ring')
-        process = os.popen(f'mrdegibbs {path}/work/{i}/dwi_raw_denoise.mif {path}/work/{i}/dwi_raw_denoise_degibbs.mif -force')
+        process = os.popen(f'mrdegibbs {path}/work/preprocess/{i}/dwi_raw_denoise.mif {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs.mif -force')
         output = process.read()
         print(output)
         process.close()
 
         # 建立AP-PA配对 /1s
-        process = os.popen(f'dwiextract {path}/work/{i}/dwi_raw.mif - -bzero | mrconvert - -coord 3 0 {path}/work/{i}/b0_PA.mif -force')
+        process = os.popen(f'dwiextract {path}/work/preprocess/{i}/dwi_raw.mif - -bzero | mrconvert - -coord 3 0 {path}/work/preprocess/{i}/b0_PA.mif -force')
         output = process.read()
         print(output)
         process.close()
-        process = os.popen(f'dwiextract {path}/work/{i}/dwi_raw.mif - -bzero | mrconvert - -coord 3 0 {path}/work/{i}/b0_AP.mif -force')
+        process = os.popen(f'dwiextract {path}/work/preprocess/{i}/dwi_raw.mif - -bzero | mrconvert - -coord 3 0 {path}/work/preprocess/{i}/b0_AP.mif -force')
         output = process.read()
         print(output)
         process.close()
-        process = os.popen(f'mrcat {path}/work/{i}/b0_PA.mif {path}/work/{i}/b0_AP.mif {path}/work/{i}/b0_pair.mif -force')
+        process = os.popen(f'mrcat {path}/work/preprocess/{i}/b0_PA.mif {path}/work/preprocess/{i}/b0_AP.mif {path}/work/preprocess/{i}/b0_pair.mif -force')
         output = process.read()
         print(output)
         process.close()
 
         # 头动矫正，变形矫正 /4m5s
         print('头动矫正，变形矫正')
-        process = os.popen(f'dwifslpreproc {path}/work/{i}/dwi_raw_denoise_degibbs.mif {path}/work/{i}/dwi_raw_denoise_degibbs_geomcorr.mif -pe_dir AP -rpe_pair -se_epi {path}/work/{i}/b0_pair.mif -eddy_options " --data_is_shelled --slm=linear --niter=5 "  -force')
+        process = os.popen(f'dwifslpreproc {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs.mif {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs_geomcorr.mif -pe_dir AP -rpe_pair -se_epi {path}/work/preprocess/{i}/b0_pair.mif -eddy_options " --data_is_shelled --slm=linear --niter=5 "  -force')
         output = process.read()
         print(output)
         process.close()
@@ -58,7 +58,7 @@ def prep(path, list):
 
         # bias场矫正 /12s
         print('bias场矫正')
-        process = os.popen(f'dwibiascorrect ants {path}/work/{i}/dwi_raw_denoise_degibbs_geomcorr.mif {path}/work/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr.mif -force')
+        process = os.popen(f'dwibiascorrect ants {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs_geomcorr.mif {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr.mif -force')
         output = process.read()
         print(output)
         process.close()
@@ -66,7 +66,7 @@ def prep(path, list):
         # 剥脑壳
         print('剥脑壳')
         process = os.popen(
-            f'dwi2mask {path}/work/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr.mif {path}/work/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr_mask.mif -force')
+            f'dwi2mask {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr.mif {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr_mask.mif -force')
         output = process.read()
         print(output)
         process.close()
@@ -74,7 +74,7 @@ def prep(path, list):
         # 优化mask
         print('优化mask')
         process = os.popen(
-            f'maskfilter {path}/work/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr_mask.mif dilate {path}/work/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr_mask_dilate6.mif -npass 6 -force')
+            f'maskfilter {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr_mask.mif dilate {path}/work/preprocess/{i}/dwi_raw_denoise_degibbs_geomcorr_biascorr_mask_dilate6.mif -npass 6 -force')
         output = process.read()
         print(output)
         process.close()
