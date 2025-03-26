58da35eb89835409033f37834cc6b6c6ca1a6caf
sunny-luoyun <2402580433@qq.com>
2025-03-26 18:06:00 +0800

    基于种子点构建纤维

---
+++ ROIFC.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+
+def FC(path, list):
+    start_time = time.time()
+    for i in list:
+        start_timee = time.time()
+        print(f"现在开始处理{i}")
+        '''flirt -in T1_coreg.nii.gz -ref MNI152.nii.gz -omat subject_to_MNI.mat'''
+        ''''''
+        process = os.popen(
+            f'')
+        output = process.read()
+        print(output)
+        process.close()
+
+        # 记录结束时间
+        end_time = time.time()
+        elapsed_time = end_time - start_timee
+        hours = int(elapsed_time // 3600)
+        minutes = int((elapsed_time % 3600) // 60)
+        seconds = int(elapsed_time % 60)
+
+        print(f"处理{i}结束，共花费时间：{hours}小时{minutes}分{seconds}秒")
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    hours = int(elapsed_time // 3600)
+    minutes = int((elapsed_time % 3600) // 60)
+    seconds = int(elapsed_time % 60)
+
+    print(f"运行结束，共花费时间：{hours}小时{minutes}分{seconds}秒")
